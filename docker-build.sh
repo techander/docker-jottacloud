495a12aedb23f63faf30a1f22a7bd11eeb7990fe
BlueT - Matthew Lien - 練喆明 <bluet@bluet.org>
2022-05-16 13:18:52 +0800

    update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
-#VERSION=0.8.62
+VERSION=0.14.58899
 
 docker build -t bluet/jottacloud .
-#docker tag bluet/jottacloud:$VERSION bluet/jottacloud:latest
-#docker push bluet/jottacloud:$VERSION
-# docker push bluet/jottacloud:latest
-# exit 0;
-
 docker scan bluet/jottacloud:latest
 
-#docker tag bluet/cronicle-docker:latest bluet/cronicle-docker:0.8.62
-#git tag "0.8.62" -a -m "docker 20.10, cronicle 0.8.62"
-#git push --tags
+docker tag bluet/jottacloud:latest bluet/jottacloud:${VERSION}
+git tag "${VERSION}" -a -m "jotta-cli ${VERSION}"
+git push --tags
+
 
 # Fixes busybox trigger error https://github.com/tonistiigi/xx/issues/36#issuecomment-926876468
-# docker run --privileged -it --rm tonistiigi/binfmt --install all
+docker run --privileged -it --rm tonistiigi/binfmt --install all
 
-# docker buildx create --use
+docker buildx create --use
 
 while true; do
         read -p "Everything ready? (We're going to build multi-platform images and push) [y/N]" yn
