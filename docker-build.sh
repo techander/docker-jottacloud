162637f046a9b8cf326f75bd93d8ee490747424e
BlueT - Matthew Lien - 練喆明 <bluet@bluet.org>
2022-06-19 06:23:06 +0800

    move tagging step to last

--- docker-build.sh
+++ docker-build.sh
@@ -7,8 +7,8 @@ docker build -t bluet/jottacloud .
 docker scan bluet/jottacloud:latest
 
 docker tag bluet/jottacloud:latest bluet/jottacloud:${VERSION}
-git tag "${VERSION}" -a -m "jotta-cli ${VERSION}"
-git push --tags
+#git tag "${VERSION}" -a -m "jotta-cli ${VERSION}"
+#git push --tags
 
 
 # Fixes busybox trigger error https://github.com/tonistiigi/xx/issues/36#issuecomment-926876468
@@ -20,9 +20,15 @@ while true; do
         read -p "Everything ready? (We're going to build multi-platform images and push) [y/N]" yn
         case $yn in
                 [Yy]* ) docker buildx build -t bluet/jottacloud:latest -t bluet/jottacloud:${VERSION}-${DATE} --platform linux/amd64,linux/arm64/v8 --push .; break;;
-                [Nn]* ) exit;;
+                [Nn]* ) break;;
                 * ) echo "";;
         esac
 done
 
+
+read -p "Tag the version of code as ${VERSION} in git? [y/N]" yn
+case $yn in
+	[Yy]* ) git tag "${VERSION}" -a -m "jotta-cli ${VERSION}" && git push --tags .; break;;
+	* ) echo "";;
+esac
 
